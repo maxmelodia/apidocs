9e66f1ab73cf0e351fd850a5c02017b18d6fe73f
Maxwell Roberto Duarte <Max1@PRO-MAX.softrom.local>
2020-01-24 16:09:53 -0300

    send new

--- apidocs/src/main/java/br/com/systems/apidocs/DemoApplication.java
+++ apidocs/src/main/java/br/com/systems/apidocs/DemoApplication.java
@@ -1,6 +1,7 @@
 //https://medium.com/@vinicius.b.martinez/microsoft-vscode-para-desenvolvedores-java-f1e9f69e6fa6
 //https://marketplace.visualstudio.com/items?itemName=afmicc.GetterAndSetterGenerator
 //https://www.receitaws.com.br/api
+
 package br.com.systems.apidocs;
 
 import org.springframework.boot.SpringApplication;
